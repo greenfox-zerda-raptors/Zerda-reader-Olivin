970919f54a214461509d96878d5f2646d8a6b736
ZSOFI PRINCZ <zsofiaprincz@gmail.com>
2017-01-16 15:16:11 +0100

    init

---
+++ Zerda-reader/src/main/java/com/zerdareader/ZerdaReaderApplication.java
@@ -0,0 +1,12 @@
+package com.zerdareader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerdaReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerdaReaderApplication.class, args);
+	}
+}
---
+++ Zerda-reader/src/test/java/com/zerdareader/ZerdaReaderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zerdareader;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZerdaReaderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
