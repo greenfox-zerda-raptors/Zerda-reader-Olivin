93fc451ca620b18245a1b32a9cff89ed7f8e4a86
zoloee <zoloee@gmail.com>
2017-01-18 16:07:43 +0100

    added populate database

--- Zerda-reader/src/main/java/com/zerdareader/ZerdaReaderApplication.java
+++ Zerda-reader/src/main/java/com/zerdareader/ZerdaReaderApplication.java
@@ -1,12 +1,25 @@
 package com.zerdareader;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZerdaReaderApplication {
+public class ZerdaReaderApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZerdaReaderApplication.class, args);
 	}
+
+	@Autowired
+	private FeedItemRepository repo;
+
+	@Override
+	public void run(String... strings) throws Exception {
+		for (int i = 0; i < 10; i++) {
+			repo.save(new FeedItem();
+		}
+	}
 }
+
