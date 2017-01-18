2bf77a68c6a09a019864bc50abbc4aeac3277a3b
zoloee <zoloee@gmail.com>
2017-01-18 23:50:53 +0100

    added: entpoints with parameters

--- Zerda-reader/src/main/java/com/zerdareader/Controller/TestJsonController.java
+++ Zerda-reader/src/main/java/com/zerdareader/Controller/TestJsonController.java
@@ -5,7 +5,9 @@ import com.zerdareader.FeedService;
 import com.zerdareader.Model.TestJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -28,6 +30,15 @@ public class TestJsonController {
     @RequestMapping(value = "/j")
     public FeedItem feedItemJson(){
         return feedService.getFeedItem(1L);
+    }
+
+    @RequestMapping(value = "/parameterrel")
+    public FeedItem feedItemJson2(@RequestParam(value="id", required=false, defaultValue="1") String id){
+        return feedService.getFeedItem(Long.parseLong(id));
+    }
+    @RequestMapping(value = "/{Id}")
+    public FeedItem feedItemJson3(@PathVariable String Id){
+        return feedService.getFeedItem(Long.parseLong(Id));
     }
 
 }
