b50a71f848afb384d612ce1c77f0ef54d4c15ba7
Rita Klebesz <ritaklebesz@gmail.com>
2017-01-19 18:06:12 +0100

    new fedd can be added, feeds can be updated

--- Zerda-reader/src/main/java/com/zerdareader/FeedUpdater.java
+++ Zerda-reader/src/main/java/com/zerdareader/FeedUpdater.java
@@ -3,6 +3,7 @@ package com.zerdareader;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 /**
  * Created by Rita on 2017-01-19.
  */
+@Component
 public class FeedUpdater {
 
     FeedReader feedReader;
@@ -21,13 +23,26 @@ public class FeedUpdater {
         this.feedReader = feedReader;
         this.feedService = feedService;
         feedsToUpdate = new ArrayList<>();
+    }
+
+    public void addNewFeed(String rssPath) {
+        TempSyndFeedStorage storage = new TempSyndFeedStorage(rssPath);
+        if (!feedService.isExist(storage)) {
+            feedService.createNewFeedItem(storage);
+        }
+    }
+
+    public void updateAllFeeds() {
         List<String> rssLinks = feedService.getAllrssLinks();
         for (String rssLink : rssLinks) {
             feedsToUpdate.add(new TempSyndFeedStorage(rssLink));
         }
+        for (TempSyndFeedStorage feed : feedsToUpdate) {
+                updateFeed(feed, feedService.getFeedBasedOnTempSFStorage(feed));
+        }
     }
 
-    public void updateFeeds(TempSyndFeedStorage tempSyndFeedStorage, Feed feed) {
+    public void updateFeed(TempSyndFeedStorage tempSyndFeedStorage, Feed feed) {
         SyndFeed syndFeed = tempSyndFeedStorage.getSyndFeed();
         if (!feedService.setPubDateByDate(syndFeed.getPublishedDate()).equals(feed.getPubDate())) {
             for (SyndEntry se : syndFeed.getEntries()) {
