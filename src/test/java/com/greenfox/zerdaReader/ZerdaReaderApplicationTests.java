b5d51df736eff63e06346aed359d28874f041f8b
Rita Klebesz <ritaklebesz@gmail.com>
2017-01-31 00:03:18 +0100

    Heroku still not working

--- src/test/java/com/greenfox/zerdaReader/ZerdaReaderApplicationTests.java
+++ src/test/java/com/greenfox/zerdaReader/ZerdaReaderApplicationTests.java
@@ -1,5 +1,6 @@
 package com.greenfox.zerdaReader;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ZerdaReaderApplicationTests {
 
-//	@Test
-//	public void contextLoads() {
-//	}
+    @Test
+    public void contextLoads() {
+    }
 }
