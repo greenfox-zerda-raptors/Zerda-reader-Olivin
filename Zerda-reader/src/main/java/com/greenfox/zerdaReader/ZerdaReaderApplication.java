0e5490062e020f1c4fd35771ab12f073203446ce
Rita Klebesz <ritaklebesz@gmail.com>
2017-01-30 22:20:45 +0100

    necessary changes for Heroku

--- Zerda-reader/src/main/java/com/greenfox/zerdaReader/ZerdaReaderApplication.java
+++ Zerda-reader/src/main/java/com/greenfox/zerdaReader/ZerdaReaderApplication.java
@@ -1,6 +1,5 @@
 package com.greenfox.zerdaReader;
 
-import com.greenfox.zerdaReader.domain.FeedsForUsers;
 import com.greenfox.zerdaReader.domain.User;
 import com.greenfox.zerdaReader.repository.FeedRepository;
 import com.greenfox.zerdaReader.repository.FeedsForUsersRepository;
@@ -12,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.transaction.Transactional;
 
 @SpringBootApplication
 public class ZerdaReaderApplication implements CommandLineRunner {
@@ -38,12 +39,13 @@ public class ZerdaReaderApplication implements CommandLineRunner {
     FeedsForUsersService feedsForUsersService;
 
     @Override
+    @Transactional
     public void run(String... strings) throws Exception {
         feedService.addNewFeed("http://index.hu/24ora/rss/");
-        feedService.addNewFeed("http://444.hu/feed");
-        feedService.addNewFeed("http://hvg.hu/rss");
-        feedService.addNewFeed("http://24.hu/feed/");
-        feedService.updateAllFeeds();
+//        feedService.addNewFeed("http://444.hu/feed");
+//        feedService.addNewFeed("http://hvg.hu/rss");
+//        feedService.addNewFeed("http://24.hu/feed/");
+//        feedService.updateAllFeeds();
 
         User testUser = new User(1234);
         userRepository.save(testUser);
@@ -51,20 +53,19 @@ public class ZerdaReaderApplication implements CommandLineRunner {
 
         testUser.getSubscribedFeeds().add(feedService.getFeed(1L));
         testUser.getSubscribedFeeds().add(feedService.getFeed(3L));
-        feedsForUsersService.populateFeedsForUsers(testUser);
+//        feedsForUsersService.populateFeedsForUsers(testUser);
         userRepository.save(testUser);
 
-        testUser = new User(567246);
-        userRepository.save(testUser);
-        testUser = userRepository.findOne(2L);
-
-        testUser.getSubscribedFeeds().add(feedService.getFeed(2L));
-        testUser.getSubscribedFeeds().add(feedService.getFeed(4L));
-        feedsForUsersService.populateFeedsForUsers(testUser);
-        userRepository.save(testUser);
-
-        feedsForUsersService.populateFeedsForUsers(testUser);
-
+//        testUser = new User(567246);
+//        userRepository.save(testUser);
+//        testUser = userRepository.findOne(2L);
+//
+//        testUser.getSubscribedFeeds().add(feedService.getFeed(2L));
+//        testUser.getSubscribedFeeds().add(feedService.getFeed(4L));
+//        feedsForUsersService.populateFeedsForUsers(testUser);
+//        userRepository.save(testUser);
+//
+//        feedsForUsersService.populateFeedsForUsers(testUser);
 
 
     }
--- Zerda-reader/src/main/java/com/greenfox/zerdaReader/utility/Updater.java
+++ Zerda-reader/src/main/java/com/greenfox/zerdaReader/utility/Updater.java
@@ -1,22 +1,16 @@
 package com.greenfox.zerdaReader.utility;
 
-import com.greenfox.zerdaReader.domain.User;
 import com.greenfox.zerdaReader.service.FeedService;
 import com.greenfox.zerdaReader.service.FeedsForUsersService;
 import com.greenfox.zerdaReader.service.UserService;
-import com.rometools.rome.io.FeedException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 /**
  * Created by zoloe on 2017. 01. 30..
  */
 
-@EnableScheduling
+//@EnableScheduling
 @Component
 public class Updater {
 
@@ -31,15 +25,14 @@ public class Updater {
         this.userService = userService;
     }
 
-    @Scheduled(fixedRate = 120000)
-    public void update() throws IOException, FeedException {
-        feedService.updateAllFeeds();
-
-        for (User user:userService.getAllUsers()) {
-            feedsForUsersService.populateFeedsForUsers(user);
-        }
-    }
-
+//    @Scheduled(fixedRate = 120000)
+//    public void update() throws IOException, FeedException {
+//        feedService.updateAllFeeds();
+//
+//        for (User user:userService.getAllUsers()) {
+//            feedsForUsersService.populateFeedsForUsers(user);
+//        }
+//    }
 
 
 }
