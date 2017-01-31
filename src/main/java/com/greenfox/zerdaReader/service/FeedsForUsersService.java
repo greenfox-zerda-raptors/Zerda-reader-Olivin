e7595f81ac39b7f0fe911072052ad879d8fd6d51
Rita Klebesz <ritaklebesz@gmail.com>
2017-01-31 11:25:35 +0100

    it runs with a bug

--- src/main/java/com/greenfox/zerdaReader/service/FeedsForUsersService.java
+++ src/main/java/com/greenfox/zerdaReader/service/FeedsForUsersService.java
@@ -2,13 +2,9 @@ package com.greenfox.zerdaReader.service;
 
 import com.greenfox.zerdaReader.domain.*;
 import com.greenfox.zerdaReader.repository.FeedsForUsersRepository;
-import com.sun.codemodel.internal.JForEach;
-import org.hibernate.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,44 +22,45 @@ public class FeedsForUsersService {
     }
 
     public ArrayList<UserFeedItemsToCustomJson> getFeedItemsForUser(User user) {
-        ArrayList<UserFeedItemsToCustomJson> l= new ArrayList<>();
-        for (FeedsForUsers fi:user.getFeedsForUsers()) {
-            l.add( new UserFeedItemsToCustomJson(fi));
+        ArrayList<UserFeedItemsToCustomJson> l = new ArrayList<>();
+        for (FeedsForUsers fi : user.getFeedsForUsers()) {
+            l.add(new UserFeedItemsToCustomJson(fi));
         }
         return l;
     }
 
     public void populateFeedsForUsers(User user) {
-        for (Feed f :user.getSubscribedFeeds()) {
-            for (FeedItem fi:f.getEntries()) {
-                if  (feedsForUsersRepository.findByUserAndFeedItem(user,fi) == null){
-                    user.getFeedsForUsers().add(new FeedsForUsers(user,fi));
+        for (Feed f : user.getSubscribedFeeds()) {
+            for (FeedItem fi : f.getEntries()) {
+                if (feedsForUsersRepository.findByUserAndFeedItem(user, fi) == null) {
+                    user.getFeedsForUsers().add(new FeedsForUsers(user, fi));
                 }
             }
         }
         feedsForUsersRepository.save(user.getFeedsForUsers());
     }
 
-    public void addFeedsForUsers(User user, FeedItem feedItem){
-        feedItem.getFeedsForUsers().add(new FeedsForUsers(user,feedItem));
+    public void addFeedsForUsers(User user, FeedItem feedItem) {
+        feedItem.getFeedsForUsers().add(new FeedsForUsers(user, feedItem));
         feedsForUsersRepository.save(feedItem.getFeedsForUsers());
     }
 
     public void populateFeedsForUsersforOneFeed(List<User> users, Feed feed) {
-        for (User user:users) {
-            for (FeedItem fi:feed.getEntries()) {
-                if  (feedsForUsersRepository.findByUserAndFeedItem(user,fi) == null){
-                    user.getFeedsForUsers().add(new FeedsForUsers(user,fi));
+        for (User user : users) {
+            for (FeedItem fi : feed.getEntries()) {
+                if (feedsForUsersRepository.findByUserAndFeedItem(user, fi) == null) {
+                    user.getFeedsForUsers().add(new FeedsForUsers(user, fi));
                 }
                 feedsForUsersRepository.save(user.getFeedsForUsers());
             }
         }
     }
+
     public void populateFeedsForUsersforFeedItems(List<User> users, List<FeedItem> feedItems) {
         for (User user : users) {
-            for (FeedItem feedItem:feedItems) {
-                if  (feedsForUsersRepository.findByUserAndFeedItem(user,feedItem) == null){
-                    user.getFeedsForUsers().add(new FeedsForUsers(user,feedItem));
+            for (FeedItem feedItem : feedItems) {
+                if (feedsForUsersRepository.findByUserAndFeedItem(user, feedItem) == null) {
+                    user.getFeedsForUsers().add(new FeedsForUsers(user, feedItem));
                 }
             }
             feedsForUsersRepository.save(user.getFeedsForUsers());
--- src/main/java/com/greenfox/zerdaReader/utility/Updater.java
+++ src/main/java/com/greenfox/zerdaReader/utility/Updater.java
@@ -1,6 +1,5 @@
 package com.greenfox.zerdaReader.utility;
 
-import com.greenfox.zerdaReader.domain.Feed;
 import com.greenfox.zerdaReader.domain.FeedItem;
 import com.greenfox.zerdaReader.domain.User;
 import com.greenfox.zerdaReader.repository.FeedItemRepository;
@@ -9,12 +8,12 @@ import com.greenfox.zerdaReader.service.FeedsForUsersService;
 import com.greenfox.zerdaReader.service.UserService;
 import com.rometools.rome.io.FeedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
-import java.time.ZoneId;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -41,8 +40,8 @@ public class Updater {
     @Scheduled(fixedRate = UPDATEFREQUENCY)
     public void update() throws IOException, FeedException {
         List<FeedItem> updatedFeedItems = feedService.updateAllFeeds();
-        for (FeedItem fi: updatedFeedItems) {
-            for (User user:fi.getFeed().getSubscribedUsers()) {
+        for (FeedItem fi : updatedFeedItems) {
+            for (User user : fi.getFeed().getSubscribedUsers()) {
 //                if (fi.getPubDate().isAfter(f.getPubDate().minusMinutes((UPDATEFREQUENCY/60000)+1)))
                 feedsForUsersService.addFeedsForUsers(user, fi);
             }
