2135c8ea402adc1bffdd2b952dcf9456ac1a110e
Rita Klebesz <ritaklebesz@gmail.com>
2017-02-01 10:44:39 +0100

    further clean up

--- src/main/java/com/greenfox/zerdaReader/service/FeedsForUsersService.java
+++ src/main/java/com/greenfox/zerdaReader/service/FeedsForUsersService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by zoloe on 2017. 01. 25..
