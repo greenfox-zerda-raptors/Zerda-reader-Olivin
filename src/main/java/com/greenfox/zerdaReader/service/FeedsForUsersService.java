89ae31b4234fce16d7c3d4026f10bd1051191289
Rita Klebesz <ritaklebesz@gmail.com>
2017-02-01 10:43:18 +0100

    further clean up

--- src/main/java/com/greenfox/zerdaReader/service/FeedsForUsersService.java
+++ src/main/java/com/greenfox/zerdaReader/service/FeedsForUsersService.java
@@ -39,38 +39,4 @@ public class FeedsForUsersService {
         }
         feedsForUsersRepository.save(user.getFeedsForUsers());
     }
-
-    public void addFeedsForUsers(User user, FeedItem feedItem) {
-        user.getFeedsForUsers().add(new FeedsForUsers(user, feedItem));
-        feedsForUsersRepository.save(user.getFeedsForUsers());
-    }
-
-    public void populateFeedsForUsersforOneFeed(List<User> users, Feed feed) {
-        for (User user : users) {
-            for (FeedItem fi : feed.getEntries()) {
-                if (feedsForUsersRepository.findByUserAndFeedItem(user, fi) == null) {
-                    user.getFeedsForUsers().add(new FeedsForUsers(user, fi));
-                }
-                feedsForUsersRepository.save(user.getFeedsForUsers());
-            }
-        }
-    }
-
-    public void populateFeedsForUsersforFeedItems(List<User> users, List<FeedItem> feedItems) {
-        for (User user : users) {
-            for (FeedItem feedItem : feedItems) {
-                if (feedsForUsersRepository.findByUserAndFeedItem(user, feedItem) == null) {
-                    user.getFeedsForUsers().add(new FeedsForUsers(user, feedItem));
-                }
-            }
-            feedsForUsersRepository.save(user.getFeedsForUsers());
-        }
-    }
-
-    public void nothing(){
-
-        int a=3;
-        int b=2;
-        
-    }
 }
