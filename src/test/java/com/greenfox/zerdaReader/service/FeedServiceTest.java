6c94a883bea0b64be1d9e073e528fc50084c3f7f
Rita Klebesz <ritaklebesz@gmail.com>
2017-02-02 11:27:18 +0100

    further cleanup

--- src/test/java/com/greenfox/zerdaReader/service/FeedServiceTest.java
+++ src/test/java/com/greenfox/zerdaReader/service/FeedServiceTest.java
@@ -1,6 +1,5 @@
 package com.greenfox.zerdaReader.service;
 
-import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.greenfox.zerdaReader.ZerdaReaderApplication;
 import org.junit.Assert;
 import org.junit.Before;
@@ -29,7 +28,6 @@ public class FeedServiceTest {
     }
 
     @Test
-    @DatabaseSetup("D:\\Green Fox Academy\\greenfox\\Zerda-reader-Olivin\\src\\test\\resources\\dataset.xml")
     public void TestAddNewFeedWithPathAlreadyInDb() throws Exception {
         long initialNumOfFeeds = feedService.feedRepo.count();
         feedService.addNewFeed("http://lorem-rss.herokuapp.com/feed?unit=second&amp;interval=30");
