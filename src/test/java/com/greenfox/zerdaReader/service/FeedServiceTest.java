7396ecb8fc6d6a7d2895f535902b030e97076cec
Rita Klebesz <ritaklebesz@gmail.com>
2017-02-02 13:09:14 +0100

    FeedService tests ready for running

--- src/test/java/com/greenfox/zerdaReader/service/FeedServiceTest.java
+++ src/test/java/com/greenfox/zerdaReader/service/FeedServiceTest.java
@@ -1,13 +1,14 @@
 package com.greenfox.zerdaReader.service;
 
 import com.greenfox.zerdaReader.ZerdaReaderApplication;
+import com.greenfox.zerdaReader.repository.FeedItemRepository;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
@@ -22,15 +23,25 @@ public class FeedServiceTest {
     @Autowired
     private FeedService feedService;
 
-    @Before
-    public void setup() {
-
-    }
+    @Autowired
+    private FeedItemRepository feedItemRepository;
 
     @Test
     public void TestAddNewFeedWithPathNotYetInDb() throws Exception {
         long initialNumOfFeeds = feedService.feedRepo.count();
+        long initialNumOfFeedItems = feedItemRepository.count();
         feedService.addNewFeed("http://lorem-rss.herokuapp.com/feed?unit=second&amp;interval=30");
         Assert.assertEquals(initialNumOfFeeds + 1, feedService.feedRepo.count());
+        Assert.assertEquals(initialNumOfFeedItems + 10, feedItemRepository.count());
+    }
+
+    @Test
+    @Sql("populateTables.sql")
+    public void TestAddNewFeedWithPathAlreadyInDb() throws Exception {
+        long initialNumOfFeeds = feedService.feedRepo.count();
+        long initialNumOfFeedItems = feedItemRepository.count();
+        feedService.addNewFeed("http://lorem-rss.herokuapp.com/feed?unit=second&amp;interval=30");
+        Assert.assertEquals(initialNumOfFeeds, feedService.feedRepo.count());
+        Assert.assertEquals(initialNumOfFeedItems, feedItemRepository.count());
     }
 }
