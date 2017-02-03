0bee49886f559004a256008425a875777e276373
Rita Klebesz <ritaklebesz@gmail.com>
2017-02-03 11:58:30 +0100

    FeedsForUsers tests done

--- src/test/java/com/greenfox/zerdaReader/service/FeedsForUsersServiceTest.java
+++ src/test/java/com/greenfox/zerdaReader/service/FeedsForUsersServiceTest.java
@@ -2,6 +2,7 @@ package com.greenfox.zerdaReader.service;
 
 import com.greenfox.zerdaReader.ZerdaReaderApplication;
 import com.greenfox.zerdaReader.domain.User;
+import com.greenfox.zerdaReader.repository.FeedRepository;
 import com.greenfox.zerdaReader.repository.UserRepository;
 import org.junit.Assert;
 import org.junit.Test;
@@ -26,6 +27,9 @@ public class FeedsForUsersServiceTest {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    FeedRepository feedRepository;
+
     @Test
     public void TestGetFeedItemsForUserShouldReturnEmptyListForNewUser() throws Exception {
         User user = new User(123);
@@ -47,8 +51,28 @@ public class FeedsForUsersServiceTest {
     }
 
     @Test
-    public void populateFeedsForUsers() throws Exception {
+    @Sql({"/clear-tables.sql", "/PopulateTables.sql"})
+    public void TestPopulateFeedsForUsersForFirstNewSubscription() throws Exception {
+        User user = userRepository.findOne(3L);
+        Assert.assertEquals(0, service.getFeedItemsForUser(user).size());
+        user.getSubscribedFeeds().add(feedRepository.findOne(2L));
+        userRepository.save(user);
+        service.populateFeedsForUsers(user);
+        userRepository.save(user);
+        user = userRepository.findOne(3L);
+        Assert.assertEquals(1, service.getFeedItemsForUser(user).size());
+    }
 
+    @Test
+    @Sql({"/clear-tables.sql", "/PopulateTables.sql"})
+    public void TestPopulateFeedsForUsersHavingSubscribedFeeds() throws Exception {
+        User user = userRepository.findOne(2L);
+        user.getSubscribedFeeds().add(feedRepository.findOne(4L));
+        userRepository.save(user);
+        service.populateFeedsForUsers(user);
+        userRepository.save(user);
+        user = userRepository.findOne(2L);
+        Assert.assertEquals(3, service.getFeedItemsForUser(user).size());
     }
 
 }
