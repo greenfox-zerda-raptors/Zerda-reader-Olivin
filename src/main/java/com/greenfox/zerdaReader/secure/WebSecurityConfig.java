3d4a6d7a9031e57821dac447d3b7f2396b9d2141
Rita Klebesz <ritaklebesz@gmail.com>
2017-02-10 12:02:20 +0100

    enable CORS

--- src/main/java/com/greenfox/zerdaReader/secure/WebSecurityConfig.java
+++ src/main/java/com/greenfox/zerdaReader/secure/WebSecurityConfig.java
@@ -25,6 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.addFilterAfter(myCustomFilter(), FilterSecurityInterceptor.class);
 
         http
+                .cors().and()
                 .csrf().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(customAuthenticationEntryPoint);
